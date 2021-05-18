5048214f8c956b618ce55a56b9b27f14078483a8
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-05-17 21:37:43 -0700

    Update some files.

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,30 @@
 from tokens import cmc_token
 
+import json
+
+import requests
+
+def write_json(data, filename = 'response.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
 def get_cmc_data(crypto):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    #symbol=BTC,ETH,XRP,BCH,EOS,LTC, xlm&CONVERT=BTC,ETH,EUR'
+    params = {'symbol': crypto, 'convert': 'USD'}
+    headers = {'X-CMC_PRO_API_KEY': cmc_token}
 
+    r = requests.get(url, headers = headers, params = params).json()
+    price = r['data'][crypto]['quote']['USD']['price']
+    #write_json(r)
+
+    #print(r)
+    return price
 
 def main():
-    pass
+    #get_cmc_data('BTC')
+    print(get_cmc_data('BTC'))
+
 
 if __name__ == '__main__':
     """
